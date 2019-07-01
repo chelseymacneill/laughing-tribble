0a4e2969ba1ce9ef86aee88dc2c59f3a2ded50b3
cmac <chelsey.macneill@gmail.com>
2019-07-01 12:26:05 -0700

    Connected mysql db to Node

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,22 @@
 // Connecting Node to MySQL
+let mysql = require("mysql");
 
+let connection = mysql.createConnection({
+host: "localhost",
+port: 3306,
+user: "root",
+password: "password",
+database: "burger_db"
+});
 
-// Export the connection
+// Initialize the connection
+connection.connect(function(error){
+    if (error) {
+        console.error("error connecting: " + error.stack);
+        return;
+    }
+    console.log("Connected as id " + connection.threadId);
+});
+
+// Export the connection for use outside of this file
+module.exports = connection;
