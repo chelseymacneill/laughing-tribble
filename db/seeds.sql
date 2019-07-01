ad4f715cf8707c13d94fd908432b39c26b489041
cmac <chelsey.macneill@gmail.com>
2019-07-01 11:40:46 -0700

    SQL schema and seeds files added

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Rob Zomburger", 1)
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Never Been Feta Burger", 0)
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Brie-oncye Burger", 1)
+
+
