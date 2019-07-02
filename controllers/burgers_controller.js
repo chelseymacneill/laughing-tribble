b24281c292891a4b323c96ca6fa9f9eb89c537f8
cmac <chelsey.macneill@gmail.com>
2019-07-01 20:38:44 -0700

    Added routing to controller.js file

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,38 @@
+// Purpose : 
+
+// Import needed modules
+let express = require("express");
+
+let burger_model = require("../models/burgers.js");
+
+// Get an instanse of router
+let router = express.Router();
+
+// Create all the routes 
+router.get("/", function(request, response){
+    burger_model.all(function(data) {
+        let handlebarsObject = {
+            // What is burgers? Property of what? 
+            burgers: data
+        };
+        console.log(handlebarsObject);
+        response.render("index", handlebarsObject)
+    });
+});
+
+// When does this get triggered? 
+router.post("api/burgers", function(request, response) {
+    burger_model.create(
+        [ "burger", "devoured"], 
+        [request.body.burger, request.body.devoured],
+        function(result) {
+            // Send back an id of the new burger
+            // What is insert ID? Where did it come from 
+            response.json({id: result.insertId});
+        });
+});
+
+
+
+// Export object to use outside of this file
+module.exports = 
