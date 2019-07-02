8588bae0a9996aab6522bdf3486196fe34771f51
cmac <chelsey.macneill@gmail.com>
2019-07-01 21:24:50 -0700

    Added logic for server.js

--- server.js
+++ server.js
@@ -1,2 +1,32 @@
 // Imports the express module
 let express = require("express");
+
+let PORT = process.env.port || 8080
+
+// Intiailize an instance of express
+let app = express();
+
+// Serve static content for the app from the "public" directory in the application directory
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Create an intance of handlebars
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exshbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+let routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on : http://localhost:" + PORT);
+});
+
