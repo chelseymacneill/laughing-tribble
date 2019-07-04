9d94ca11e959e1874e6e532f8f5f370c6aab64e0
cmac <chelsey.macneill@gmail.com>
2019-07-03 17:49:00 -0700

    Added Jaws connection info to connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,18 @@ password: "password",
 database: "burgers_db"
 });
 
+// Adding Jaws DB
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'z4hr74uw9ujzjwfx',
+        password: 'jckjbjqkvky79pbt',
+        database: 'bw1gr4d7dn6cykz8'
+    });
+};
+
 // Initialize the connection
 connection.connect(function(error){
     if (error) {
